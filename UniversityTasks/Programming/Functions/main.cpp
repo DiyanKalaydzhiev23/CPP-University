cf1e182cb83b59abf68e228b8bbaa9a85aa1d1a1
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-12-14 17:41:22 +0200

    Added more solutions

--- UniversityTasks/Programming/Functions/main.cpp
+++ UniversityTasks/Programming/Functions/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 #include<bits/stdc++.h>
 
 using std::cout;
 using std::cin;
 using std::vector;
 using std::string;
+using std::regex;
+using std::abs;
+using std::regex_replace;
 using std::endl;
 
 inline void addElementToList(const string & element, vector<string> & list) {
@@ -49,6 +53,87 @@ void findWagons(int startIndex, int endIndex, const string & composition) {
     cout << endl;
 }
 
+void zigZagMatrixFill(int **matrix, int rows, int cols) {
+    for (int row = 0; row < rows; row++)
+        if (row % 2  == 0)
+            for (int col = 0; col < cols; col++)
+                cin >> matrix[row][col];
+        else
+            for (int col = cols - 1; col >= 0; col--)
+                cin >> matrix[row][col];
+}
+
+void printMatrix(int **matrix, int rows, int cols) {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            cout << matrix[row][col] << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void deleteAllNumbersInAString(string & row) {
+    for (char i = '0'; i <= '9'; i++)
+        row.erase(std::remove(row.begin(), row.end(), i), row.end());
+}
+
+inline void replaceSubString(string & toReplace, string & newString, string & text) {
+    text = regex_replace(text, regex(toReplace), newString);
+}
+
+inline string clearAllRepeatingCharsInAString(string & text) {
+    string temp;
+
+    temp += text.at(0);
+
+    for (int i = 1; i < text.length(); i++)
+        if (temp.find(text.at(i)) >= temp.length())
+            temp += text.at(i);
+
+    return temp;
+}
+
+inline int countSameSymbolsInAString(string & text1, string & text2) {
+    int sameSymbolsCounter = 0;
+
+    string temp1 = clearAllRepeatingCharsInAString(text1);
+    string temp2 = clearAllRepeatingCharsInAString(text2);
+
+    if (temp1.length() < temp2.length()) {
+        for (char chr : temp1)
+            if (temp2.find(chr) < temp2.length())
+                sameSymbolsCounter++;
+    } else {
+        for (char chr : temp2)
+            if (temp1.find(chr) < text2.length())
+                sameSymbolsCounter++;
+    }
+
+    return sameSymbolsCounter;
+}
+
+inline int countDifferentSymbolsInAString(string & text1, string & text2) {
+    int diffSymbolsCounter = 0;
+
+    string temp1 = clearAllRepeatingCharsInAString(text1);
+    string temp2 = clearAllRepeatingCharsInAString(text2);
+
+    if (temp1.length() < temp2.length()) {
+        for (char chr : temp1)
+            if (temp2.find(chr) >= temp2.length())
+                diffSymbolsCounter++;
+    } else {
+        for (char chr : temp2)
+            if (temp1.find(chr) >= text2.length())
+                diffSymbolsCounter++;
+    }
+
+    return diffSymbolsCounter;
+}
+
+
+
 int main() {
     vector<string> list;
 
@@ -74,6 +159,54 @@ int main() {
     cin >> endIndex;
 
     findWagons(startIndex, endIndex, composition);
+
+    // task 4
+
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    int **matrix = new int *[rows];
+
+    for (int row = 0; row < rows; row++)
+        matrix[row] = new int[cols];
+
+    zigZagMatrixFill(matrix, rows, cols);
+    printMatrix(matrix, rows, cols);
+
+    delete[] matrix[rows];
+
+  // task 5
+    string text = "9c10he4t38e6n9e7";
+
+    deleteAllNumbersInAString(text);
+
+    cout << text << endl;
+
+    // task 6
+    string toReplace = "te";
+    string newString = "sa";
+
+    replaceSubString(toReplace, newString, text);
+
+    cout << text << endl;
+
+    // task 7 & 8
+    string text1;
+    string text2;
+
+    cout << "Enter the first text: ";
+    cin >> text1;
+
+    cout << "Enter the second text: ";
+    cin >> text2;
+
+    cout << "Count of same symbols: " << countSameSymbolsInAString(text1, text2) << endl;
+    cout << "Count of different symbols: " << countDifferentSymbolsInAString(text1, text2) << endl;
 
     return 0;
 }
